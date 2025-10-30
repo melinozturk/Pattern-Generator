b6f09d2f380458bb26e5b1f95c0b70623a29154a
melinozturk <MelinOzturk0707@gmail.com>
2025-10-30 15:02:14 +0000

    lalalallalalala

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,49 @@
+//Melin Ozturk
+//10/30/2025
+//This program will produce different patterns based on the methods being called by using while, do ... while and for loops.
 
+public class Pattern{
+    //pre condition :  Each method written should accept an argument that will determine how many lines of the pattern are shown on the screen. 
+    //postcondition
+    public static void stars(int rows){ // use while loops
+       // code goes here
+       int count = 1;
+       while (count<=7);{
+        System.out.println("*")
+        count+=2
+       }
+    }
+      public static void main(int rows){
+       // testing goes here
+    }
+     public static void triangle(int rows){ // use do ... while or while loops
+       // code goes here
+    }
+      public static void main(int rows){
+       // testing goes here
+    }
+     public static void odds(int start){ // use for loops
+       // code goes here
+    }
+      public static void main(int rows){
+       // testing goes here
+    }
+      public static void eo(int maxE){ // use loops of your choice
+        // code goes here
+    }
+      public static void main(int rows){
+       // testing goes here
+    }
+     public static void pyramid(int rows){
+       // code goes here
+    }
+      public static void main(int rows){
+       // testing goes here
+    }
+
+
+
+
+
+
+}
